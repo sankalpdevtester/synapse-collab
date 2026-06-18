a7fd63cc9acb6f84773d1fd5b9232b99e35cad12
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:52:27 +0530

    feat: add collaboration route

--- src/routes/collaboration.ts
+++ src/routes/collaboration.ts
@@ -1,38 +1,17 @@
 import express, { Request, Response } from 'express';
-import { CRDTImpl } from 'src/utils/crdt';
-import { Document } from 'src/utils/documentSync';
 import { WebSocket } from 'ws';
+import { CRDT } from '../utils/crdt';
 
 const router = express.Router();
 
-router.post('/crdt', (req: Request, res: Response) => {
-  const crdt = req.body;
-  const document = new Document();
-  const crdtImpl = new CRDTImpl(document);
-  crdt.forEach((crdtItem: CRDT) => {
-    crdtImpl.applyCRDT(crdtItem);
-  });
-  res.json(crdtImpl.getCRDTs());
+router.get('/collaboration', (req: Request, res: Response) => {
+  const ws = new WebSocket('ws://localhost:8080');
+  const crdt = new CRDT(ws, new (class DocumentSync {
+    updateDocument(document: string) {
+      console.log('Document updated:', document);
+    }
+  })());
+  res.send('Collaboration route');
 });
 
-router.get('/document', (req: Request, res: Response) => {
-  const document = new Document();
-  res.json(document.getText());
-});
-
-router.websocket('/collaboration', (ws: WebSocket) => {
-  ws.on('message', (message) => {
-    const crdt = JSON.parse(message.toString());
-    ws.send(JSON.stringify(crdt));
-  });
-
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
-
-  ws.on('error', (error) => {
-    console.log('Error occurred:', error);
-  });
-});
-
-export { router };
+export default router;
